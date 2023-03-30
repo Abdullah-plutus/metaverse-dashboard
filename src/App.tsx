b035cddb5da903edd97b60e56ec83ccf6fdb3ec8
Abdullah-plutus <YOUR_EMAIL>
2023-03-30 12:58:33 +0500

    feat: adding initial directory structure

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Home } from "./container";
+
+function App() {
+  return <div className="App">Home</div>;
+}
+
+export default App;
---
+++ src/container/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CHome } from "../components";
+
+function Home() {
+  return (
+    <>
+      <CHome />
+    </>
+  );
+}
+
+export default Home;
